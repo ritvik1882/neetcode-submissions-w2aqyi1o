593ce2b1436e4b03aec32d07394cf1279ccb9f3a
Ritvik <61428433+ritvik1882@users.noreply.github.com>
2026-04-08 03:15:32 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = {}; // val -> index
+
+        for (let i = 0; i < nums.length; i++){
+            const complement = target - nums[i];
+            if (map[complement] !== undefined){
+                return [map[complement], i];
+            }
+            map[nums[i]] = i;
+        }
+        
+    }
+}
